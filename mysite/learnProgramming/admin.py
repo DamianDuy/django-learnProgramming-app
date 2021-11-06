6ba524b819c2356e04e845520c9f83d839821dc8
DamianDuy <gamergray8@gmail.com>
2021-11-06 12:07:27 +0100

    Add administration panel setup

--- mysite/learnProgramming/admin.py
+++ mysite/learnProgramming/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+admin.site.register(Programming_Language)
+admin.site.register(Subject)
+admin.site.register(Test)
+admin.site.register(Question)
+admin.site.register(Answer)
+admin.site.register(User_Answer)
+admin.site.register(Answers_Data)
+admin.site.register(Subject_Point)
+admin.site.register(Test_Point)
+admin.site.register(Question_Point)
+admin.site.register(Rating)
