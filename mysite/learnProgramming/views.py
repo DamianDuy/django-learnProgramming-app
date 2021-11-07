474f0e1aa4898031e637e847bd22875d239b6e1b
mczerepowicz <mczerepowicz@sigma.ug.edu.pl>
2021-11-07 22:18:48 +0100

    Add index view and urls.py

--- mysite/learnProgramming/views.py
+++ mysite/learnProgramming/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import *
 
 # Create your views here.
+
+
+def index(request):
+    programming_language_list = Programming_Language.objects.all()
+    context = {'programming_language_list': programming_language_list}
+    return render(request, 'learnProgramming/index.html', context)
