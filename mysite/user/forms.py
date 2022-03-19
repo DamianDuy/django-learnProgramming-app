218cede01ca1f2e26604db59c81bd6605a24072e
DamianDuy <gamergray8@gmail.com>
2022-03-19 16:43:19 +0100

    Add boxes for GDPR and Terms and Conditions

--- mysite/user/forms.py
+++ mysite/user/forms.py
@@ -6,6 +6,8 @@ from django.utils.safestring import mark_safe
 class SignUpForm(UserCreationForm):
 
     email = forms.EmailField(max_length=254, help_text='Required. Provide a valid email address.')
+    rodo_confirmed = forms.BooleanField(required=True, label=mark_safe('Accept <a href="/GDPR">GDPR</a>'))
+    terms_and_conditions_confirmed = forms.BooleanField(required=True, label=mark_safe('Accept <a href="/terms"> Terms and Conditions </a>'))
 
     class Meta:
         model = User
