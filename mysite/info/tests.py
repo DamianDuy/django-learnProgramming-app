3ef2f2069bcf3fcfcded9d82dd3700922b8c11ef
DamianDuy <gamergray8@gmail.com>
2022-04-08 21:11:03 +0200

    Add unit tests for info app

--- mysite/info/tests.py
+++ mysite/info/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+from .models import *
 
 # Create your tests here.
+
+class AboutModelTest(TestCase):
+    def setUp(self) -> None:
+        aboutPage1 = About.objects.create(content="test content")
+        #return super().setUp()
+    def test_about_content(self):
+        aboutPage = About.objects.get(id=1)
+        self.assertEqual(aboutPage.content, "test content")
+        self.assertNotEqual(aboutPage.content, "test string")
